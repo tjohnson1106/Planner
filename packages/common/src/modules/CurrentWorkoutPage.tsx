31266272d8b30a82aa68560fb5cb3e4a4973c02d
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-21 13:15:26 -0500

    created current workout page module

---
+++ packages/common/src/modules/CurrentWorkoutPage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+interface Props {}
+
+export const CurrentWorkoutPage: React.FC<Props> = () => {
+  return (
+    <View>
+      <Text>Current Workout Page</Text>
+    </View>
+  );
+};
