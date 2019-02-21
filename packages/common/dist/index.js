6eb228ea562acdea700c28bd6f2676cecccee0c1
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-21 13:38:48 -0500

    build for web

--- packages/common/dist/index.js
+++ packages/common/dist/index.js
@@ -1,39 +1,20 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importStar(require("react"));
+var react_1 = __importDefault(require("react"));
 var react_native_1 = require("react-native");
-var mobx_react_lite_1 = require("mobx-react-lite");
-var CounterStore_1 = require("./stores/CounterStore");
-exports.App = mobx_react_lite_1.observer(function () {
-    var counterStore = react_1.useContext(CounterStore_1.CounterStoreContext);
+var Router_1 = require("./Router");
+exports.App = function () {
     return (react_1.default.createElement(react_native_1.View, { style: styles.container },
-        react_1.default.createElement(react_native_1.Text, { style: styles.welcome }, "Planner"),
-        react_1.default.createElement(react_native_1.Text, { style: styles.instructions }, "Just a hooks example"),
-        react_1.default.createElement(react_native_1.Text, { style: styles.instructions }, counterStore.count),
-        react_1.default.createElement(react_native_1.Button, { title: "increment", onPress: function () { return counterStore.count++; } })));
-});
+        react_1.default.createElement(Router_1.Router, null)));
+};
 var styles = react_native_1.StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#F5FCFF"
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: "center",
-        margin: 10
-    },
-    instructions: {
-        textAlign: "center",
-        color: "#333333",
-        marginBottom: 5
     }
 });
